604622fb39844cb4f3ba54a30aa10dfd44444466
Victor Olin <victor.vieweg@icloud.com>
2021-01-30 00:41:44 +0100

    Forgot to show window

--- Startup.cs
+++ Startup.cs
@@ -69,6 +69,10 @@ namespace Minesweeper
                 Resizable = false,
                 Maximizable = false
             });
+
+            await browserWindow.WebContents.Session.ClearCacheAsync();
+
+            browserWindow.OnReadyToShow += () => browserWindow.Show();
         }
     }
 }
