a6d8625050495bb825428d2c98a736398ad58048
Victor Olin <victor.vieweg@icloud.com>
2021-01-31 15:42:34 +0100

    Generates full field. Bugs on neighbouring cells.

--- Data/Cell.cs
+++ Data/Cell.cs
@@ -2,14 +2,26 @@ using System;
 
 namespace Minesweeper.Data {
     public enum BoxType {
-        Bomb,
         Empty,
+        Bomb,
         Neighbour
     }
 
     public class Cell {
-        public BoxType Type { get; set; }
+        public BoxType Type { get; set; } // is BoxType.Emty by default
         public bool Revealed { get; set; }
         public int NeighbouringBombs { get; set; }
+
+        public string SquareContent
+        {
+            get
+            {
+                switch (Type) {
+                    case BoxType.Bomb: return "B";
+                    case BoxType.Neighbour: return NeighbouringBombs.ToString();
+                    default: return "";
+                }
+            }
+        }
     }
 }
--- Data/GridService.cs
+++ Data/GridService.cs
@@ -11,17 +11,91 @@ namespace Minesweeper.Data
         public Task<Cell[,]> GetGrid()
         {
             var rnd = new Random();
-            var grid = new Cell[hrows, vrows];
-            for (int i = 0; i < hrows; i++)
-            {
-                for (int j = 0; j < vrows; j++)
-                {
-                    grid[i, j].NeighbouringBombs = rnd.Next(10);
-                }
-            }
+            var grid = new Cell[vrows, hrows];
+            PopulateGridWithBombs(ref grid, rnd);
+            FillRemainingGrid(ref grid);
             return Task.FromResult(
                 grid
             );
+        }
+
+        private void PopulateGridWithBombs(ref Cell[,] grid, Random rnd)
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    // 36/(50*30)=0,024 - estimated 36 bombs on full grid
+                    // each square has a 2.4% chance of being a bomb
+                    var cell = new Cell();
+                    var bombPoss = rnd.Next(1000);
+                    if (bombPoss < 25)
+                    {
+                        cell.Type = BoxType.Bomb;
+                    }
+                    if (i == 3 && j == 3)
+                    {
+                        cell.Type = BoxType.Bomb;
+                    }
+                    grid[i, j] = cell;
+                }
+            }
+        }
+
+        private void FillRemainingGrid(ref Cell[,] grid)
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    PopulateNeighbour(ref grid, i, j);
+                }
+            }
+        }
+
+        private void PopulateNeighbour(ref Cell[,] grid, int i, int j)
+        {
+            var nearBombs = 0;
+
+            (int b, int f)[] offsets = Perimeter(i, j, grid.GetLength(0), grid.GetLength(1));
+
+            try
+            {
+                for (int x = offsets[0].b; x <= offsets[0].f; x++)
+                {
+                    for (int y = offsets[1].b; y <= offsets[1].f; y++)
+                    {
+                        var xp = i + x; //xprime
+                        var yp = j + y; //yprime
+
+                        if (!(xp == i && yp == j))
+                            if (grid[xp, yp].Type == BoxType.Bomb)
+                                nearBombs++;
+                    }
+                }
+            }
+            catch (Exception){}
+
+            if (nearBombs != 0)
+            {
+                grid[i, j].Type = BoxType.Neighbour;
+                grid[i, j].NeighbouringBombs = nearBombs;
+            }
+        }
+
+        private (int b, int f)[] Perimeter(int i, int j, int maxh, int maxw) // i = height offset, j = width offset
+        {
+            (int h, int w)[] offset = { (0, 0), (0, 0) }; //[0] = height offset, [1] = width offset
+
+            if (i == 0) offset[0] = (0, 1); //0 steps up, 1 step up
+            else if (i == maxh - 1) offset[0] = (-1, 0);
+            else offset[0] = (-1, 1);
+
+            if (j == 0) offset[1] = (0, 1);
+            else if (j == maxw - 1) offset[1] = (-1, 0);
+            else offset[1] = (-1, 1);
+
+            return offset;
         }
     }
 }
