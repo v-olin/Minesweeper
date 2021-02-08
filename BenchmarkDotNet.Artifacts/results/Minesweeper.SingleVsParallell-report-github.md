bacc8577a242956303fc24a5c8d4a886be95c943
Victor Olin <victor.vieweg@icloud.com>
2021-02-08 23:14:34 +0100

    Benchmarked other algorithm to reveal neighbours

--- BenchmarkDotNet.Artifacts/results/Minesweeper.SingleVsParallell-report-github.md
+++ BenchmarkDotNet.Artifacts/results/Minesweeper.SingleVsParallell-report-github.md
@@ -1,13 +1,14 @@
 ``` ini
 
-BenchmarkDotNet=v0.12.1.20210204-develop, OS=Windows 10.0.18363.1316 (1909/November2019Update/19H2)
-Intel Core i5-1035G7 CPU 1.20GHz, 1 CPU, 8 logical and 4 physical cores
+BenchmarkDotNet=v0.12.1.20210208-develop, OS=Windows 10.0.19041.746 (2004/May2020Update/20H1)
+Intel Core i5-9600K CPU 3.70GHz (Coffee Lake), 1 CPU, 6 logical and 6 physical cores
 .NET SDK=5.0.101
-  [Host]     : .NET 5.0.1 (5.0.120.57516), X64 RyuJIT DEBUG  [AttachedDebugger]
+  [Host]     : .NET 5.0.1 (5.0.120.57516), X64 RyuJIT DEBUG
   DefaultJob : .NET 5.0.1 (5.0.120.57516), X64 RyuJIT
 
 
 ```
-|   Method |     Mean |    Error |   StdDev |   Median |
-|--------- |---------:|---------:|---------:|---------:|
-| AsSingle | 69.40 ns | 2.670 ns | 7.308 ns | 67.32 ns |
+|   Method |     Mean |    Error |   StdDev |
+|--------- |---------:|---------:|---------:|
+| AsSingle | 92.53 ns | 0.294 ns | 0.245 ns |
+| AsSamuel | 45.10 ns | 0.027 ns | 0.024 ns |
--- Startup.cs
+++ Startup.cs
@@ -12,8 +12,8 @@ using Microsoft.Extensions.Hosting;
 using ElectronNET.API;
 using ElectronNET.API.Entities;
 using Minesweeper.Data;
-// using BenchmarkDotNet.Attributes;
-// using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Minesweeper
 {
@@ -62,12 +62,13 @@ namespace Minesweeper
                 ElectronBootstrap();
             }
 
-            // var summary = BenchmarkRunner.Run<SingleVsParallell>();
-            // System.Console.WriteLine(summary.Reports);
+            var summary = BenchmarkRunner.Run<SingleVsParallell>();
+            System.Console.WriteLine(summary.Reports);
         }
-    
-        public async void ElectronBootstrap() {
-            
+
+        public async void ElectronBootstrap()
+        {
+
             var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
             {
                 Width = 1350,
@@ -85,23 +86,33 @@ namespace Minesweeper
         }
     }
 
-    public class SingleVsParallell{
+    public class SingleVsParallell
+    {
 
-        private Cell[,] grid;
+        private Cell[,] grid = new Cell[10000, 5000];
         private List<(int i, int j)> emptyNeighbours;
-        private int i = 25;
-        private int j = 15;
+        private int i;
+        private int j;
+        private Queue<(int i, int j)> toVisit;
+        private HashSet<(int i, int j)> visited;
 
-        public SingleVsParallell(){
+        public SingleVsParallell()
+        {
             emptyNeighbours = new List<(int i, int j)>();
-            InitGrid();
+            toVisit = new Queue<(int i, int j)>();
+            visited = new HashSet<(int i, int j)>();
+            i = (int)(grid.GetLength(0) / 2);
+            j = (int)(grid.GetLength(1) / 2);
+            for (int a = 0; a < grid.GetLength(0); a++)
+            {
+                for (int b = 0; b < grid.GetLength(1); b++)
+                {
+                    grid[a, b] = new Cell() { Type = BoxType.Empty };
+                }
+            }
         }
 
-        private async void InitGrid(){
-            grid = await new GridService().GetGrid();
-        }
-
-        // [Benchmark] // Lägg till benchmarks för AsParallell och för samuels metod
+        [Benchmark]
         public void AsSingle()
         {
             emptyNeighbours.Add((i, j));
@@ -128,6 +139,54 @@ namespace Minesweeper
             }
         }
 
+        [Benchmark]
+        public void AsQueue()
+        {
+            toVisit.Enqueue((i, j));
+            while (toVisit.Count != 0)
+            {
+                var temp = toVisit.Dequeue();
+                visited.Add((i, j));
+                if (!visited.Contains(temp))
+                {
+                    List<(int i, int j)> neighbours = Neighbours(i, j, grid.GetLength(0), grid.GetLength(1));
+                    foreach (var neighbour in neighbours)
+                    {
+                        if (grid[neighbour.i, neighbour.j].Type == BoxType.Empty &&
+                            !(neighbour.i == i && neighbour.j == j) &&
+                            !visited.Contains(neighbour))
+                        {
+                            toVisit.Enqueue(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+
+        private List<(int i, int j)> Neighbours(int i, int j, int maxh, int maxw)
+        {
+            int[] iOffsets, jOffsets;
+
+            if (i == 0) iOffsets = new int[] { 0, 1 }; //Enumerable.Range(0,2).ToList();
+            else if (i == maxh - 1) iOffsets = new int[] { -1, 0 };
+            else iOffsets = new int[] { -1, 0, 1 };
+
+            if (j == 0) jOffsets = new int[] { 0, 1 };
+            else if (j == maxw - 1) jOffsets = new int[] { -1, 0 };
+            else jOffsets = new int[] { -1, 0, 1 };
+
+            List<(int i, int j)> ns = new();
+
+            foreach (var ip in iOffsets)
+            {
+                foreach (var jp in jOffsets)
+                {
+                    ns.Add((i + ip, j + jp));
+                }
+            }
+
+            return ns;
+        }
 
         private (int b, int f)[] Perimeter(int i, int j, int maxh, int maxw) // i = height offset, j = width offset
         {
@@ -143,8 +202,5 @@ namespace Minesweeper
 
             return offset;
         }
-
-        
-
     }
 }
